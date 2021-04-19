0e5e2bf56b1158d427b113c6b3e8dfe9aa0a80be
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-19 17:41:17 +0900

    [Feature] - Created Config file

---
+++ src/main/java/com/recogition_project/face_recognition/config/projectConfig.java
@@ -0,0 +1,16 @@
+package com.recogition_project.face_recognition.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class projectConfig implements WebMvcConfigurer{
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/", "classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+}
