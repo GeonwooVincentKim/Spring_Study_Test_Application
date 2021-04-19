e92deceb85706cdfbe880b14ffd7c296c3abe328
Geonwoo Kim <kw.kim@xenoimpact.com>
2021-04-19 17:27:59 +0900

    [Update] - Connected to the different file, but same directory

--- src/main/java/com/recogition_project/face_recognition/web/SampleController.java
+++ src/main/java/com/recogition_project/face_recognition/web/SampleController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -16,10 +18,13 @@ public class SampleController {
         return "index.html";
     }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
+    @GetMapping("/main")
+    public String hello(){
+    // public String hello(@RequestParam(name="name", required=false) String name, Model model) {
+        // model.addAttribute("name", name);
+        return "main.html";
+        // return "/src/main/resources/config/templates/main.html";
+        // return "../main.html";
         // return "Hello VS Code";
         // return "/src/main/resources/config/templates/main.html";
     }
